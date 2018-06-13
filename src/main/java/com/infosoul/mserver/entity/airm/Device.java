b2eb79f6360633c97f057e495323675806ec8162
longxy <1871954195@qq.com>
2018-06-14 00:19:21 +0800

    连接池改为druid

---
+++ src/main/java/com/infosoul/mserver/entity/airm/Device.java
@@ -0,0 +1,13 @@
+package com.infosoul.mserver.entity.airm;
+
+import java.io.Serializable;
+
+/**
+ * 设备表
+ *
+ * @author longxy
+ * @date 2018-06-14 0:03
+ */
+public class Device implements Serializable{
+
+}
